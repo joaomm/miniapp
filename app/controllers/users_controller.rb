a38bf72685ab1a16147665e18cb78b5b8102e2ff
joaomm <joaomm88@gmail.com>
2012-11-09 13:50:34 -0200

    Simple refactoring on users_controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,15 +1,23 @@
 class UsersController < ApplicationController 
+  before_filter :fetch_user
+  before_filter :fetch_task_list, only: [:mark_task_list_as_favorite, :unmark_task_list_as_favorite]
+  
   def mark_task_list_as_favorite
-    @user = User.find(params[:user_id])
-    @task_list = TaskList.find(params[:task_list_id])
     @user.mark_task_list_as_favorite(@task_list)
     respond_with(@task_list.user, @task_list)
   end
   
   def unmark_task_list_as_favorite
-    @user = User.find(params[:user_id])
-    @task_list = TaskList.find(params[:task_list_id])
     @user.unmark_task_list_as_favorite(@task_list)
     respond_with(@task_list.user, @task_list)
   end
+  
+  private
+    def fetch_user
+      @user = User.find(params[:user_id])
+    end
+    
+    def fetch_task_list
+      @task_list = TaskList.find(params[:task_list_id])
+    end
 end
